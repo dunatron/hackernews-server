43c01929fd8ea04fecdb7e7d8d7468a9d6569650
Heath Dunlop <heath@silverstripe.com>
2018-09-05 01:21:23 +1200

    ff

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,55 @@
-function feed(parent, args, context, info) {
-  return context.db.query.links({}, info)
+// function feed(parent, args, context, info) {
+//   return context.db.query.links({}, info)
+// }
+
+// function feed(parent, args, context, info) {
+//   const where = args.filter
+//     ? {
+//         OR: [
+//           { url_contains: args.filter },
+//           { description_contains: args.filter },
+//         ],
+//       }
+//     : {}
+
+//   return context.db.query.links(
+//     { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+//     info
+//   )
+// }
+
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+        OR: [
+          { url_contains: args.filter },
+          { description_contains: args.filter },
+        ],
+      }
+    : {}
+
+  // 1
+  const queriedLinks = await context.db.query.links(
+    { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
+    `{ id }`,
+  )
+
+  // 2
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `
+
+  const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+  // 3
+  return {
+    count: linksConnection.aggregate.count,
+    linkIds: queriedLinks.map(link => link.id),
+  }
 }
 
 function info(parent, args, context, info) {
